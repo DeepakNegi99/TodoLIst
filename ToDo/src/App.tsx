d36a4811ce842cd09996229d4792f60c56d29fde
Deepak Negi <deepak99negi@gmail.com>
2025-09-04 23:14:23 +0530

    Initial Layout
    Initial Layout

--- ToDo/src/App.tsx
+++ ToDo/src/App.tsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
+import "./App.css";
 
-import './App.css'
+const App = () => {
+  const [value, setvalue] = useState([])
 
-function App() {
- 
-
+  const submitForm = (event: React.FormEvent) => {
+    event.preventDefault();
+    alert("Form submitted");
+  }
   return (
-    <div>
-      Hello World
+    <div className="app">
+      <div className="todoBox">
+        <h1>ToDo List</h1>
+        <form onSubmit={submitForm}>
+          <input type="text" name="name" placeholder="Add your new todo" />
+          <button type="submit">Add</button>
+          <ul>
+            <li>sample li</li>
+          </ul>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
