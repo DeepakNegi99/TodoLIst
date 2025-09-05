461fbda55c0713195fe82adbb3f91f892dbd60d4
Deepak Negi <deepak99negi@gmail.com>
2025-09-05 22:48:48 +0530

    added itemList
    added itemList

--- ToDo/src/App.tsx
+++ ToDo/src/App.tsx
@@ -15,9 +15,7 @@ const App = () => {
         <form onSubmit={submitForm}>
           <input type="text" name="name" placeholder="Add your new todo" />
           <button type="submit">Add</button>
-          <ul>
-            <li>sample li</li>
-          </ul>
+          <ItemsList />
         </form>
       </div>
     </div>
@@ -25,3 +23,16 @@ const App = () => {
 }
 
 export default App;
+
+
+const ItemsList = () => {
+  return (
+    <div>
+      <ul>
+        <li>Item 1 <button>Delete</button></li>
+        <li>Item 2 <button>Delete</button></li>
+        <li>Item 3 <button>Delete</button></li>
+      </ul>
+    </div>
+  )
+}
